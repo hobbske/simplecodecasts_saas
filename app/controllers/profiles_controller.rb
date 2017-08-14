84dda2280eb21104b63e503e867d3166affe13af
Kevin Hobbs <kevin21921@gmail.com>
2017-08-14 15:00:31 -0400

    Added before filters to correct correct user.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,8 @@
 class ProfilesController < ApplicationController
 
+	before_action :authenticate_user!
+	before_action :only_current_user
+
 	def new
 		
 		@user = User.find( params[:user_id] )
@@ -26,11 +29,28 @@ class ProfilesController < ApplicationController
 		@profile = @user.profile
 	end
 
+	def update
+		@user = User.find( params[:user_id] )
+		@profile = @user.profile
+		if @profile.update_attributes(profile_params)
+			redirect_to user_path( params[:user_id] )
+			flash[:success] = "Profile Updated!"
+		else
+			render action: :edit
+		end
+
+	end
+
 	private
 
 		def profile_params
-			params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-			
+			params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)	
+		end
+
+		def only_current_user
+			@user = User.find( params[:user_id] )
+			redirect_to(root_url) unless @user == current_user
+			flash[:danger] = 'You are not authorized to access that page.'
 		end
 
 end
